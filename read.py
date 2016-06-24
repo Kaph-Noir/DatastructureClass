cb5bcaa3c2ee93268d46f7aa058a3ec690cf3673
Phark Noi <Phark Noi>
2016-06-24 16:15:52 +0900

    node 시각화

--- read.py
+++ read.py
@@ -1,9 +1,13 @@
 f = open('sm_friend.txt')
+print (f)
 
+node =''
 for line in f:
-    line = line[0:-1]
-    print(type(line))
-print(type(f))
+    if line.strip():
+        line = line[0:-1]
+        node += line
+        print(node)
+
 
 '''
 def read():
